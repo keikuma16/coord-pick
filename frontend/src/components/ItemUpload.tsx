7f1ba4f5ce32565057b980a6c1c4aaa8039d655b
keita <keeeeeeeta900@gmail.com>
2026-03-13 18:00:10 +0900

    ItemUploadのmin-hを80vhに変更

--- frontend/src/components/ItemUpload.tsx
+++ frontend/src/components/ItemUpload.tsx
@@ -68,48 +68,47 @@ export const ItemUpload = () => {
         console.log(items);
     }
     return(
-        <div className="flex justify-center items-center min-h-[70vh]">
-            <form onSubmit={handleSubmit} className="bg-gray-50 rounded-2xl p-3 w-full max-w-xl my-auto">
+        <div className="flex justify-center items-center min-h-[80vh]">
+            <form onSubmit={handleSubmit} className="bg-gray-50 rounded-2xl p-3 w-full max-w-xl">
                 <h2 className="text-center text-2xl text-black font-bold">投稿</h2>
-                    <div className="p-2 flex items-center">
-                        <label className="text-black w-24 shrink-0">Styling説明</label>
-                        <input type="text" value={explanation} onChange={(e) => setExplanation(e.target.value)} className="grow border border-blackborder rounded-sm ml-2"/>
-                    </div>
-                    <div className="p-2 flex items-center">
-                        <label className="text-black w-24 shrink-0">写真</label>
-                        <input type="file" onChange={(e) =>{ 
-                            if(e.target.files && e.target.files[0]){
-                                setImageUrl(e.target.files[0])
-                            }
-                        }} className="grow border border-black rounded-sm ml-2 min-w-0"/>
-                    </div>
-                    <div className="p-2 flex items-center">
-                       <label className="text-black w-24 shrink-0">商品名</label> 
-                       <input type="text" value={itemname} onChange={(e) => setItemname(e.target.value)} className="grow border border-black rounded-sm ml-2"/>
-                    </div>
-                    <div className="p-2 flex items-center">
-                        <label className="text-black w-24 shrink-0">カテゴリー</label>
-                        <input type="text" value={category} onChange={(e) => setCategory(e.target.value)} className="grow border border-black rounded-sm ml-2"/>
-                    </div>
-                    <div className="p-2 flex items-center">
-                        <label className="text-black w-24 shrink-0">ブランド</label>
-                        <input type="text" value={brand} onChange={(e) => setBrand(e.target.value)} className="grow border border-black rounded-sm ml-2"/>
-                    </div>
-                    <div className="p-2 flex items-center">
-                        <label className="text-black w-24 shrink-0">商品URL</label>
-                        <input type="text" value={itemurl} onChange={(e) => setItemurl(e.target.value)} className="grow border border-black rounded-sm ml-2"/>
-                    </div>
-                    <button type="button" onClick={addItem} className="text-black px-3 border border-black rounded-sm ml-2">商品を追加</button>
-                    <ul>
-                        {items.map((item,index) => (
-                            <li key={index}>{item.name}</li>
-                        ))}
-                    </ul>
-
-                    <div className="flex justify-center items-center">
-                        <button type="submit" className="text-gray-50 font-bold px-3 rounded-2xl bg-sky-600  ml-2 mt-4 w-2xl h-10">出品</button>
-                    </div> 
-                </form>
-            </div>
+                <div className="p-2 flex items-center">
+                    <label className="text-black w-24 shrink-0">Styling説明</label>
+                    <input type="text" value={explanation} onChange={(e) => setExplanation(e.target.value)} className="grow border border-black rounded-sm ml-2"/>
+                </div>
+                <div className="p-2 flex items-center">
+                    <label className="text-black w-24 shrink-0">写真</label>
+                    <input type="file" onChange={(e) =>{ 
+                        if(e.target.files && e.target.files[0]){
+                            setImageUrl(e.target.files[0])
+                        }
+                    }} className="grow border border-black rounded-sm ml-2 min-w-0"/>
+                </div>
+                <div className="p-2 flex items-center">
+                    <label className="text-black w-24 shrink-0">商品名</label> 
+                    <input type="text" value={itemname} onChange={(e) => setItemname(e.target.value)} className="grow border border-black rounded-sm ml-2"/>
+                </div>
+                <div className="p-2 flex items-center">
+                    <label className="text-black w-24 shrink-0">カテゴリー</label>
+                    <input type="text" value={category} onChange={(e) => setCategory(e.target.value)} className="grow border border-black rounded-sm ml-2"/>
+                </div>
+                <div className="p-2 flex items-center">
+                    <label className="text-black w-24 shrink-0">ブランド</label>
+                    <input type="text" value={brand} onChange={(e) => setBrand(e.target.value)} className="grow border border-black rounded-sm ml-2"/>
+                </div>
+                <div className="p-2 flex items-center">
+                    <label className="text-black w-24 shrink-0">商品URL</label>
+                    <input type="text" value={itemurl} onChange={(e) => setItemurl(e.target.value)} className="grow border border-black rounded-sm ml-2"/>
+                </div>
+                <button type="button" onClick={addItem} className="text-black px-3 border border-black rounded-sm ml-2">商品を追加</button>
+                <ul>
+                    {items.map((item,index) => (
+                        <li key={index}>{item.name}</li>
+                    ))}
+                </ul>
+                <div className="flex justify-center items-center">
+                    <button type="submit" className="text-gray-50 font-bold px-3 rounded-2xl bg-sky-600  ml-2 mt-4 w-2xl h-10">出品</button>
+                </div> 
+            </form>
+        </div>
     )
 }
